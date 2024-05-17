2e3e9a6760f067fab66d1bfff0f2df42c4f75f87
unknown <ajgelber@gmail.com>
2024-05-17 17:13:22 -0500

    Adds music script and autoplay via code

--- scenes/GameScene/Music.gd
+++ scenes/GameScene/Music.gd
@@ -1,9 +1,25 @@
 extends Node
 
+@export var descent_music: AudioStream = null
+@export var ascent_music: AudioStream = null
+
+@onready var music_player: AudioStreamPlayer = $MusicPlayer
+
+
+func play_descent():
+	music_player.stop()
+	music_player.stream = descent_music
+	music_player.play()
+	
+func play_ascent():
+	music_player.stop()
+	music_player.stream = ascent_music
+	music_player.play()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	play_descent()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
