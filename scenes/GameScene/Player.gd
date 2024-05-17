e7242cf5fa709f4da5a5d18dc6b85b5afbc5d938
wmgroot <wmgroot@gmail.com>
2024-05-17 16:41:54 -0500

    embiggen the sub

--- scenes/GameScene/Player.gd
+++ scenes/GameScene/Player.gd
@@ -6,13 +6,15 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var on_station = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
 
 func _physics_process(delta):
 	# Add the gravity.
