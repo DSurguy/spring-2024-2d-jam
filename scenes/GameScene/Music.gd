ced122c430b1d652ee951bc56303363e254718bf
unknown <ajgelber@gmail.com>
2024-05-17 17:08:12 -0500

    Ascent music and mixer

---
+++ scenes/GameScene/Music.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@export var descent_music: AudioStream = null
+@export var ascent_music: AudioStream = null
+
+@onready var music_player: AudioStreamPlayer = $MusicPlayer
+
+
+func play_descent():
+	music_player.stop()
+	music_player.stream = descent_music
+	music_player.play()
+	
+func play_ascent():
+	music_player.stop()
+	music_player.stream = ascent_music
+	music_player.play()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play_descent()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
