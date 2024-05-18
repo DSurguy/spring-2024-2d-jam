d4d9b547327ff2dc061c7da87b3e7cb8889c380b
iwilbanks <abqaiqbean@yahoo.com>
2024-05-18 16:37:03 -0500

    added station sprites and animations

--- scenes/GameScene/Stations/Station.gd
+++ scenes/GameScene/Stations/Station.gd
@@ -4,11 +4,14 @@ var active = false
 var submarine: Node2D
 @onready var audio: StationAudio = $StationAudio
 
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	submarine = get_parent().get_parent().get_parent()
 	if submarine.name != "Submarine":
 		print("Submarine not found for station: " + name)
+	animationPlayer.stop(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,10 +23,12 @@ func activate_station():
 	active = true
 	get_parent().activate()
 	audio.play_open()
+	animationPlayer.play("active")
 	print("station active")
 	
 func deactivate_station():
 	active = false
 	get_parent().deactivate()
 	audio.play_close()
+	animationPlayer.stop(true)
 	print("station inactive")
