1c59dd2a693038d813395d64f83ff2eabfe149cc
Derek Surguy <Surguy.Derek@gmail.com>
2024-05-18 11:02:11 -0500

    Update tilemap collision, spawn wall plants

--- scenes/GameScene/Plants/PlantDirector.gd
+++ scenes/GameScene/Plants/PlantDirector.gd
@@ -4,15 +4,21 @@ extends Node2D
 @export var camera: Camera2D
 
 var generic_plant_scene: PackedScene = load("res://scenes/GameScene/Plants/GenericPlant.tscn")
-var first_spawn_time_seconds = 1
-var has_done_first_spawn: bool = false
-var spawn_time_seconds = 8
-var seconds_since_last_spawn: float = 0
+var wall_plant_scene: PackedScene = load("res://scenes/GameScene/Plants/WallPlant/WallPlant.tscn")
 var random = RandomNumberGenerator.new()
-var temp_x_clamp = 1000
-var spawn_y_offset_mult = 5
-var spawn_y_offset_const = 100
-var clump_radius = 100
+
+@export var floating_plant_first_spawn_time_seconds = 1
+@export var floating_plant_spawn_time_seconds = 8
+@export var floating_plant_seconds_since_last_spawn: float = 0
+@export var wall_plant_first_spawn_time_seconds = 1
+@export var wall_plant_spawn_time_seconds = 15
+@export var wall_plant_seconds_since_last_spawn: float = 0
+@export var spawn_y_offset_mult = 15
+@export var spawn_y_offset_const = 100
+@export var clump_radius = 100
+var floating_plant_has_done_first_spawn: bool = false
+var wall_plant_has_done_first_spawn: bool = false
+var temp_x_clamp = 500
 
 func _ready():
 	assert(submarine_node != null, "submarine node is required")
@@ -23,23 +29,30 @@ func _process(delta):
 		_handle_descent_process(delta)
 
 func _handle_descent_process(delta):
-	seconds_since_last_spawn += delta
-	if not has_done_first_spawn and seconds_since_last_spawn > first_spawn_time_seconds:
-		spawn_plants()
-		has_done_first_spawn = true
-	if seconds_since_last_spawn > spawn_time_seconds:
-		seconds_since_last_spawn = 0
-		spawn_plants()
+	# TODO: Actually make this garbage spawn off screen
+	var view_rect = get_viewport_rect().size * camera.zoom
+	var bottom = camera.get_screen_center_position().y + view_rect.y / 2
+	var spawn_y_position = bottom + spawn_y_offset_const + submarine_node.descent_speed * spawn_y_offset_mult
+	$Rays.position.y = spawn_y_position
+	spawn_floating_plants(delta, spawn_y_position)
+	spawn_wall_plants(delta, spawn_y_position)
 
-func spawn_plants():
+func spawn_floating_plants(delta: float, spawn_y_position: float):
+	
+	floating_plant_seconds_since_last_spawn += delta
+	if not floating_plant_has_done_first_spawn and floating_plant_seconds_since_last_spawn > floating_plant_first_spawn_time_seconds:
+		floating_plant_has_done_first_spawn = true
+		floating_plant_seconds_since_last_spawn = 0
+	elif floating_plant_seconds_since_last_spawn > floating_plant_spawn_time_seconds:
+		floating_plant_seconds_since_last_spawn = 0
+	else: return
+	
+	# TODO: Clamp x with raycast
+	
 	var num_clusters = random.randi_range(1, 3)
 	for cluster in num_clusters:
 		var clump_x_position = random.randi_range(0 - temp_x_clamp, 0 + temp_x_clamp)
-		# TODO: Actually make this garbage spawn off screen
-		var view_rect = get_viewport_rect().size * camera.zoom
-		var bottom = camera.get_screen_center_position().y + view_rect.y / 2
-		var clump_y_position = bottom + spawn_y_offset_const + submarine_node.descent_speed * spawn_y_offset_mult
-		var clump_position = Vector2(clump_x_position, clump_y_position)
+		var clump_position = Vector2(clump_x_position, spawn_y_position)
 		var num_plants = random.randi_range(1, 3)
 		for plant in num_plants:
 			var plant_position = Vector2(
@@ -50,5 +63,27 @@ func spawn_plants():
 			new_plant.position = clump_position + plant_position
 			add_child(new_plant)
 
-func spawn_wall_plants():
-	pass
+func spawn_wall_plants(delta: float, spawn_y_position: float):
+	wall_plant_seconds_since_last_spawn += delta
+	if not wall_plant_has_done_first_spawn and wall_plant_seconds_since_last_spawn > wall_plant_first_spawn_time_seconds:
+		wall_plant_has_done_first_spawn = true
+		wall_plant_seconds_since_last_spawn = 0
+	elif wall_plant_seconds_since_last_spawn > wall_plant_spawn_time_seconds:
+		wall_plant_seconds_since_last_spawn = 0
+	else: return
+	
+	var num_plants = random.randi_range(1, 3)
+	
+	var new_plant: WallPlant = wall_plant_scene.instantiate()
+	var left: bool = random.randi_range(0,1) == 1
+	if left:
+		var collider = $Rays/LeftRay.get_collider()
+		var collision_point = Vector2i($Rays/LeftRay.get_collision_point() + Vector2(2, 0))
+		new_plant.position = collision_point - Vector2i(collision_point.x % 32, collision_point.y % 32) + Vector2i(16, 16)
+	else:
+		var collider = $Rays/RightRay.get_collider()
+		var collision_point = Vector2i($Rays/RightRay.get_collision_point() + Vector2(-2, 0))
+		new_plant.position = collision_point - Vector2i(collision_point.x % 32, collision_point.y % 32) + Vector2i(16, 16)
+		new_plant.face_left = true
+	
+	add_child(new_plant)
--- scenes/GameScene/Plants/WallPlant/WallPlant.gd
+++ scenes/GameScene/Plants/WallPlant/WallPlant.gd
@@ -2,6 +2,7 @@ class_name WallPlant extends Node2D
 
 @export var segments: int = 3
 @export var harvested: bool = false
+@export var face_left: bool = false
 
 func _ready():
 	$WallPlantRoot.attach_to_next_part($WallPlantStalk)
@@ -11,4 +12,3 @@ func _ready():
 
 func harvest():
 	harvested = true
-
