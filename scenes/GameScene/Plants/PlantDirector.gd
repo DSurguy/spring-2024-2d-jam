07e24dfcc35b2e3224e107cf59c240424bda3441
Derek Surguy <Surguy.Derek@gmail.com>
2024-05-17 20:48:40 -0500

    Spawn plants yay

--- scenes/GameScene/Plants/PlantDirector.gd
+++ scenes/GameScene/Plants/PlantDirector.gd
@@ -1,18 +1,22 @@
 extends Node2D
 
 @export var submarine_node: Submarine
+@export var camera: Camera2D
 
 var generic_plant_scene: PackedScene = load("res://scenes/GameScene/Plants/GenericPlant.tscn")
-var spawn_time_seconds = 2
+var first_spawn_time_seconds = 1
+var has_done_first_spawn: bool = false
+var spawn_time_seconds = 8
 var seconds_since_last_spawn: float = 0
 var random = RandomNumberGenerator.new()
-var temp_x_clamp = 500
-var spawn_y_offset_mult = 3
-var spawn_y_offset_const = 10
+var temp_x_clamp = 1000
+var spawn_y_offset_mult = 5
+var spawn_y_offset_const = 100
 var clump_radius = 10
 
 func _ready():
 	assert(submarine_node != null, "submarine node is required")
+	assert(camera != null, "camera is required")
 
 func _process(delta):
 	if submarine_node.descending:
@@ -20,6 +24,9 @@ func _process(delta):
 
 func _handle_descent_process(delta):
 	seconds_since_last_spawn += delta
+	if not has_done_first_spawn and seconds_since_last_spawn > first_spawn_time_seconds:
+		spawn_plants()
+		has_done_first_spawn = true
 	if seconds_since_last_spawn > spawn_time_seconds:
 		seconds_since_last_spawn = 0
 		spawn_plants()
@@ -27,8 +34,12 @@ func _handle_descent_process(delta):
 func spawn_plants():
 	var num_clusters = random.randi_range(1, 3)
 	for cluster in num_clusters:
-		var x = random.randi_range(submarine_node.position.x - temp_x_clamp, submarine_node.position.x + temp_x_clamp)
-		var clump_position = Vector2(submarine_node.position.y * spawn_y_offset_mult + spawn_y_offset_const)
+		var clump_x_position = random.randi_range(0 - temp_x_clamp, 0 + temp_x_clamp)
+		# TODO: Actually make this garbage spawn off screen
+		var view_rect = get_viewport_rect().size * camera.zoom
+		var bottom = camera.get_screen_center_position().y + view_rect.y / 2
+		var clump_y_position = bottom + spawn_y_offset_const + submarine_node.descent_speed * spawn_y_offset_mult
+		var clump_position = Vector2(clump_x_position, clump_y_position)
 		var num_plants = random.randi_range(1, 5)
 		for plant in num_plants:
 			var plant_position = Vector2(
@@ -37,3 +48,4 @@ func spawn_plants():
 			)
 			var new_plant = generic_plant_scene.instantiate()
 			new_plant.position = clump_position + plant_position
+			add_child(new_plant)
