d099c35450d326664857f7af1abe35656645bb89
Derek Surguy <Surguy.Derek@gmail.com>
2024-05-18 15:52:23 -0500

    Create proper animations for all floating plants

--- resources/PlantData.gd
+++ resources/PlantData.gd
@@ -1,4 +1,6 @@
 class_name PlantData extends Resource
 
-@export var texture: Texture = null
+enum ANIMATION_STRATEGY { ALGAE, WALL_STALK, WALL_LEAF, BULB }
+
 @export var value: int = 0
+@export var animation: ANIMATION_STRATEGY = ANIMATION_STRATEGY.ALGAE
--- scenes/GameScene/Plants/GenericPlant.gd
+++ scenes/GameScene/Plants/GenericPlant.gd
@@ -1,6 +1,11 @@
 class_name GenericPlant extends Node2D
 
-@onready var animation_tree : AnimationTree = $AnimationTree
+var animation_algae: PackedScene = load("res://scenes/GameScene/Plants/AlgaeAnimation.tscn")
+var animation_wall_stalk: PackedScene = load("res://scenes/GameScene/Plants/WallStalkAnimation.tscn")
+var animation_wall_leaf: PackedScene = load("res://scenes/GameScene/Plants/WallLeafAnimation.tscn")
+var animation_bulb: PackedScene = load("res://scenes/GameScene/Plants/BulbAnimation.tscn")
+
+var animation_tree : AnimationTree
 @export var data: PlantData
 @export var initial_velocity: Vector2 = Vector2((randf() - .5) * 0.5, (randf() - .5) * 0.5)
 
@@ -8,12 +13,29 @@ var velocity:Vector2
 
 func _ready():
 	assert(data != null, "PlantData is required")
-	$Sprite2D.texture = data.texture
-	animation_tree.active = true
+	create_animation()
 	var scaleFactor = 1 + (1.5 * (randf() - 0.25))
 	scale = Vector2(scaleFactor, scaleFactor)
 	velocity = initial_velocity
 	rotation_degrees = randf() * 360.0
+
+func create_animation():
+	var animation_scene: Node2D
+	match data.animation:
+		PlantData.ANIMATION_STRATEGY.ALGAE:
+			animation_scene = animation_algae.instantiate()
+		PlantData.ANIMATION_STRATEGY.WALL_STALK:
+			animation_scene = animation_wall_stalk.instantiate()
+		PlantData.ANIMATION_STRATEGY.WALL_LEAF:
+			animation_scene = animation_wall_leaf.instantiate()
+		PlantData.ANIMATION_STRATEGY.BULB:
+			animation_scene = animation_bulb.instantiate()
+	
+	animation_scene.name = "Animation"
+	add_child(animation_scene)
+	animation_tree = $Animation.get_node("AnimationTree")
+	animation_tree.active = true
+	pass
 
 func _process(delta):
 	position += velocity
