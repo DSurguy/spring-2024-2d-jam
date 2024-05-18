c72e543599950c2ab73b3070031ea5b3ea5eaec7
Derek Surguy <Surguy.Derek@gmail.com>
2024-05-18 09:53:19 -0500

    Finish up live plant physics

---
+++ scenes/GameScene/Plants/WallPlant/WallPlantStalk.gd
@@ -0,0 +1,12 @@
+class_name WallPlantStalk extends RigidBody2D
+
+func attach_to_next_part(next_part: PhysicsBody2D):
+	$PinJoint2D.node_a = self.get_path()
+	$PinJoint2D.node_b = next_part.get_path()
+
+func _ready():
+	apply_force(Vector2(0, -200))
+
+func _physics_process(delta):
+	apply_force(Vector2(0, -200))
+	pass
