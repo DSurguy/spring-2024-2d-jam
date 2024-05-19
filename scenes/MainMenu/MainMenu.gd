39b9cb1afbd52ad469cb118d17c8c89dd5878fc4
Derek Surguy <Surguy.Derek@gmail.com>
2024-05-19 09:13:07 -0500

    Enable controller on main menu

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -1,5 +1,9 @@
 extends LoadableScene
 
+func _ready():
+	$CanvasLayer/MainMenu/VBoxContainer/CenterContainer/VBoxContainer/LoadGameScene.grab_focus()
+	load_complete.emit()
+
 func _on_load_test_scene_button_pressed():
 	loader.load_scene("res://scenes/TestScene/TestScene.tscn")
 	pass # Replace with function body.
