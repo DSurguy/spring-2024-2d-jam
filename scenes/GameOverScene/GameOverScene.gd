aacd12283ce46f7bf1569fcaff19acbbc0d692f1
unknown <ajgelber@gmail.com>
2024-05-19 10:43:53 -0500

    Death BWAAAAH

--- scenes/GameOverScene/GameOverScene.gd
+++ scenes/GameOverScene/GameOverScene.gd
@@ -1,5 +1,9 @@
 extends LoadableScene
 
+@onready var button: Button = $CanvasLayer/Control/VBoxContainer/Button
+
+func _ready():
+	button.grab_focus()
 
 func _on_button_pressed():
 	loader.load_scene("res://scenes/MainMenu/MainMenu.tscn")
