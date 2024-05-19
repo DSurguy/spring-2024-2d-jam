836496c48ab0655597918a327e1a8463a15d00c3
wmgroot <wmgroot@gmail.com>
2024-05-19 10:14:49 -0500

    turtles release that delicious oxy

--- scenes/GameScene/Turtle.gd
+++ scenes/GameScene/Turtle.gd
@@ -5,6 +5,10 @@ var current_plant_eating:GenericPlant
 var eating_timer = 0
 var eating_time = 2
 
+var bubble_emitter: PackedScene = load("res://scenes/particles/BubbleEmitter.tscn")
+var bubble_timer = 0
+var bubble_time = 0.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready()
@@ -14,9 +18,18 @@ func _ready():
 func _process(delta):
 	super._process(delta)
 	
+	if bubble_timer > 0:
+		bubble_timer -= delta
+	
 	if edible_plants.size() > 0:
 		if current_plant_eating == edible_plants[0] && not current_plant_eating.is_queued_for_deletion():
 			# same plant, carry on eating
+			if bubble_timer <= 0:
+				var emitter = bubble_emitter.instantiate()
+				emitter.position = position
+				get_tree().get_root().add_child(emitter)
+				bubble_timer = bubble_time
+			
 			eating_timer += delta
 			if eating_timer > eating_time:
 				_eat_plant(current_plant_eating)
