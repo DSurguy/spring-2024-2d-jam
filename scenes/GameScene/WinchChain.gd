fcc3a049642cf406387ad77102aa9d62c2fe6a58
Derek Surguy <Surguy.Derek@gmail.com>
2024-05-19 10:30:49 -0500

    Adjust chain to look less stupid

--- scenes/GameScene/WinchChain.gd
+++ scenes/GameScene/WinchChain.gd
@@ -4,5 +4,5 @@ extends Node2D
 @export var attach_to: Node2D
 
 func _process(delta):
-	$Line2D.points[0] = attach_from.position
+	$Line2D.points[0] = attach_from.position - Vector2(0, 32)
 	$Line2D.points[1] = attach_to.position
