72583bd5f260f823194a627ad27882b2c43982bf
unknown <ajgelber@gmail.com>
2024-05-19 09:37:30 -0500

    Main menu sfx

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -1,21 +1,30 @@
 extends LoadableScene
+@onready var sfx: UISFX = $Uisfx
 
 func _ready():
 	$CanvasLayer/MainMenu/VBoxContainer/CenterContainer/VBoxContainer/LoadGameScene.grab_focus()
 	load_complete.emit()
 
 func _on_load_test_scene_button_pressed():
-	loader.load_scene("res://scenes/TestScene/TestScene.tscn")
-	pass # Replace with function body.
+	sfx.accept()
+	loader.load_scene("res://scenes/TestScene/TestScene.tscn")	
 
 func _on_load_game_scene_button_pressed():
-	loader.load_scene("res://scenes/GameScene/GameScene.tscn")
-	pass # Replace with function body.
+	sfx.accept()
+	loader.load_scene("res://scenes/GameScene/GameScene.tscn")	
 
 func _on_load_shop_scene_button_pressed():
-	loader.load_scene("res://scenes/ShopScene/ShopScene.tscn")
-	pass # Replace with function body.
+	sfx.accept()
+	loader.load_scene("res://scenes/ShopScene/ShopScene.tscn")	
 
 func _on_load_ui_test_scene_button_pressed():
-	loader.load_scene("res://scenes/UI/UITestScene.tscn")
-	pass # Replace with function body.
+	sfx.accept()
+	loader.load_scene("res://scenes/UI/UITestScene.tscn")	
+
+
+func _on_load_shop_scene_mouse_entered():
+	sfx.mouseover()
+
+
+func _on_load_game_scene_mouse_entered():
+	sfx.mouseover()
